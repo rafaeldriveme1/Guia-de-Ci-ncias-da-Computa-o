47aa78dde1219a832f6636fa5c3a08bb18ce729a
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-03-10 00:50:12 -0300

    feat: C helloworld e scanf

---
+++ 1-periodo/Linguagem-C/Curso/02-scanf/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    int numero;
+	
+	printf("Entre com um numero:");
+    scanf("%d", &numero);
+    getchar();
+
+    printf("Seu numero e : %d \n\n", numero);
+	
+	return 0;
+}
